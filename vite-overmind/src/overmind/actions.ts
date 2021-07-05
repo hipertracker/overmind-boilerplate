252d31fb9822a07ae7ad3a76a555809f93a7b3b8
Jaroslaw Zabiello <hipertracker@gmail.com>
2021-07-05 09:06:34 +0100

    add onInitialize example

--- vite-overmind/src/overmind/actions.ts
+++ vite-overmind/src/overmind/actions.ts
@@ -1,5 +1,9 @@
 import { Context } from '../overmind';
 
+export const onInitializeOvermind = ({ state }: Context): void => {
+  state.counter = 10;
+}
+
 export const inc = ({ state }: Context): void => {
   state.counter += 1;
 };
